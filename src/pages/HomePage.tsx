36e34070e26eda756dd2884bf13f3048021f9148
murdoch <murdochkhallz@gmail.com>
2024-11-25 16:28:28 +0100

    First commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,19 @@
-import "./homePageStyle.css"
+import "./homePage.style.css"
+import {ProgressBar} from "../components/ProgressBar";
+import {Choice} from "../components/Choice";
 export const HomePage =()=>{
     return(
         <div className={"container"}>
-            <p>Home</p>
+            <div>
+
+                <ProgressBar/>
+                <h2>Test your level in TI!</h2>
+                <p>What is the name of the computer created in 1985?</p>
+                <Choice/>
+                <Choice/>
+                <Choice/>
+            </div>
+
 
         </div>
 
