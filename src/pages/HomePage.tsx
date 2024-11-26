50bb220d21cd2af8111603b873a92ea3ac36526c
murdoch <murdochkhallz@gmail.com>
2024-11-26 17:43:18 +0100

    Layout setup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,31 +3,9 @@ import {ProgressBar} from "../components/ProgressBar";
 import {Choice} from "../components/Choice";
 import {useState} from "react";
 import {finalResultMessage} from "../utils/finalResultMessage";
+import {questions} from "../utils/questions";
 
-interface Question {
-    question: string;
-    answers: string[];
-    correctAnswer: string;
-}
 
-const questions: Question[] = [
-    {
-        question: 'What is the capital of France?',
-        answers: ['Berlin', 'Madrid', 'Paris', 'Lisbon'],
-        correctAnswer: 'Paris',
-    },
-    {
-        question: 'Which planet is known as the Red Planet?',
-        answers: ['Earth', 'Mars', 'Jupiter', 'Saturn'],
-        correctAnswer: 'Mars',
-    },
-    {
-        question: 'Who wrote "Romeo and Juliet"?',
-        answers: ['Mark Twain', 'Charles Dickens', 'William Shakespeare', 'Jane Austen'],
-        correctAnswer: 'William Shakespeare',
-    },
-    // Add more questions as needed
-];
 
 
 export const HomePage =()=>{
@@ -108,76 +86,84 @@ export const HomePage =()=>{
 
 
     return(
-        <div className={"container"}>
-            <div>
+        <div className={"wrapper"}>
+            <div></div>
+            <div className={"container"}>
+                <div>
 
-                <ProgressBar progressPercentage={progressPercentage} />
-                <h1>Test your level in TI!</h1>
-                {/*<p>What is the name of the computer created in 1985?</p>*/}
-                {/*<Choice/>*/}
-                {/*<Choice/>*/}
-                {/*<Choice/>*/}
+                    <ProgressBar progressPercentage={progressPercentage}/>
+                    <h1>Test your level in TI!</h1>
+                    {/*<p>What is the name of the computer created in 1985?</p>*/}
+                    {/*<Choice/>*/}
+                    {/*<Choice/>*/}
+                    {/*<Choice/>*/}
 
                     {/*<p style={{textAlign: 'center'}}>{`Question ${currentQuestionIndex + 1} of ${totalQuestions}`}</p>*/}
-                {!quizFinished &&
-                    <p style={{textAlign: 'center'}}>{`Question ${currentQuestionIndex + 1} of ${totalQuestions}`}</p>}
+                    {!quizFinished &&
+                        <p style={{textAlign: 'center'}}>{`Question ${currentQuestionIndex + 1} of ${totalQuestions}`}</p>}
 
 
-                {quizFinished ? (
-                    <div>
-                        <p>Your score: {score} / {questions.length} ({Math.round((score/questions.length)*100)}%)</p>
-                        <p>{finalResultMessage(score, questions.length )}</p>
-                        {/*<p>🎉 Congratulations! You're a computer expert!</p>*/}
-
-                        <button className={"btn--medium"} onClick={handleRestartQuiz}>Restart Quiz</button>
-                    </div>
-                ) : (
-                    <div>
-                        <p>{questions[currentQuestionIndex].question}</p>
+                    {quizFinished ? (
                         <div>
-                            {
-                                questions[currentQuestionIndex].answers.map((answer, index) => {
-                                let buttonClass = 'answer-button';
-                                // Determine the button's class based on selection and correctness
-                                if (selectedAnswer === answer) {
-                                    buttonClass += answer === questions[currentQuestionIndex].correctAnswer ? ' correct' : ' incorrect';
-                                } else if (correctAnswer === answer) {
-                                    buttonClass += ' correct'; // Highlight the correct answer
-                                }else{
-                                    buttonClass="answer-button"
-                                }
-                                return(
-                                <div
-                                    key={index}
-                                    // className={`answer-button ${selectedAnswer === answer ? 'selected' : ''}`}
-                                    className={buttonClass}
-                                    // onChange={() => {
-                                    //     handleAnswerSelection(answer)
-                                    // }}
-                                    onClick={() =>
-                                        handleAnswerSelection(answer)
-                                    }
-                                >
-                                    {/*<label>*/}
-                                    {/*    <input*/}
-                                    {/*        type="radio"*/}
-                                    {/*        value={answer}*/}
-                                    {/*        checked={selectedAnswer === answer}*/}
-                                    {/*        onChange={() => handleAnswerSelection(answer)}*/}
-                                    {/*    />*/}
-                                        {answer}
-                                    {/*</label>*/}
-                                </div>
-                            )})
-                            }
+                            <p>Your
+                                score: {score} / {questions.length} ({Math.round((score / questions.length) * 100)}%)</p>
+                            <p>{finalResultMessage(score, questions.length)}</p>
+                            {/*<p>🎉 Congratulations! You're a computer expert!</p>*/}
+
+                            <button className={"btn--medium"} onClick={handleRestartQuiz}>Restart Quiz</button>
                         </div>
-                        {/*<button onClick={handleNextQuestion} disabled={!selectedAnswer}>*/}
-                        {/*    Next Question*/}
-                        {/*</button>*/}
-                    </div>
-                )}
+                    ) : (
+                        <div>
+                            <p>{questions[currentQuestionIndex].question}</p>
+                            <div>
+                                {
+                                    questions[currentQuestionIndex].answers.map((answer, index) => {
+                                        let buttonClass = 'answer-button';
+                                        // Determine the button's class based on selection and correctness
+                                        if (selectedAnswer === answer) {
+                                            buttonClass += answer === questions[currentQuestionIndex].correctAnswer ? ' correct' : ' incorrect';
+                                        } else if (correctAnswer === answer) {
+                                            buttonClass += ' correct'; // Highlight the correct answer
+                                        } else {
+                                            buttonClass = "answer-button"
+                                        }
+                                        return (
+                                            <div
+                                                key={index}
+                                                // className={`answer-button ${selectedAnswer === answer ? 'selected' : ''}`}
+                                                className={buttonClass}
+                                                // onChange={() => {
+                                                //     handleAnswerSelection(answer)
+                                                // }}
+                                                onClick={() =>
+                                                    handleAnswerSelection(answer)
+                                                }
+                                            >
+                                                {/*<label>*/}
+                                                {/*    <input*/}
+                                                {/*        type="radio"*/}
+                                                {/*        value={answer}*/}
+                                                {/*        checked={selectedAnswer === answer}*/}
+                                                {/*        onChange={() => handleAnswerSelection(answer)}*/}
+                                                {/*    />*/}
+                                                {answer}
+                                                {/*</label>*/}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                            {/*<button onClick={handleNextQuestion} disabled={!selectedAnswer}>*/}
+                            {/*    Next Question*/}
+                            {/*</button>*/}
+                        </div>
+                    )}
+                </div>
+
+
             </div>
 
+            <div></div>
 
         </div>
 
