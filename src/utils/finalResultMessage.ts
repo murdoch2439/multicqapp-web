c1bec532578f2f1ba972703ab9241968fb394317
murdoch <murdochkhallz@gmail.com>
2024-11-26 17:43:01 +0100

    Layout setup

--- src/utils/finalResultMessage.ts
+++ src/utils/finalResultMessage.ts
@@ -1,14 +1,15 @@
 export const finalResultMessage=(score:number, totalQuestions:number)=>{
     const totalScoreInPercentage = Math.round((score / totalQuestions) * 100)
-
-    if( totalScoreInPercentage < 50 ){
-        return `😔 You still have a lot to learn, keep practicing!`
-    }else if( totalScoreInPercentage === 50 ){
-        return `🎉 Congratulations! You're have a solid foundation knowledge in computer!`
-    }else if( totalScoreInPercentage > 50   ){
-        return `🎉 Congratulations! You're crashing the game!`
-    }else if( totalScoreInPercentage > 75  ){
+    if( totalScoreInPercentage === 100  ){
         return `🏆 Congratulations! You're a computer expert!`
+    }if( totalScoreInPercentage >= 75  ){
+        return `🏆 Congratulations! You're a computer expert!`
+    } if( totalScoreInPercentage === 50 ){
+        return `🎉 You have a solid foundation knowledge you can improve on!`
+    } if( totalScoreInPercentage > 50   ){
+        return `🎉 Congratulations! You're crashing the game!`
+    }if( totalScoreInPercentage < 50 ){
+        return `😔 You still have a lot to learn, keep practicing!`
     }
     else{
         return `🎉 Congratulations! You're a computer expert!`
