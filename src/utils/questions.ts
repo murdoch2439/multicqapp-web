273f203906f1f5c50073082be3f88d3dcf6906f6
murdoch <murdochkhallz@gmail.com>
2024-11-26 17:43:23 +0100

    Layout setup

---
+++ src/utils/questions.ts
@@ -0,0 +1,30 @@
+interface Question {
+    question: string;
+    answers: string[];
+    correctAnswer: string;
+}
+
+
+export const questions: Question[] = [
+    {
+        question: 'What was the name of the first electronic computer, built in 1946?',
+        answers: ['TERMOX', 'MADAV', 'ENIAC', 'EDVAC'],
+        correctAnswer: 'ENIAC',
+    },
+    {
+        question: 'Which operating system was developed by Linus Torvalds?',
+        answers: ['Windows', 'macOs', 'Android', 'Linux'],
+        correctAnswer: 'Linux',
+    },
+    {
+        question: 'What protocol is used to transfer files over the web?',
+        answers: ['HTML', 'HTTP', 'FTP', 'SMTP'],
+        correctAnswer: 'FTP',
+    },
+    {
+        question: "What's the main difference between IPv4 and IPv6?",
+        answers: ['Security','IP address size' , 'Data encryption', 'Connection speed'],
+        correctAnswer: 'IP address size',
+    },
+    // Add more questions as needed
+];
