58e87bb801dc38f8408f67fe8a89519360ced9e0
murdoch <murdochkhallz@gmail.com>
2024-11-26 18:33:37 +0100

    Adding footer to the project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import {DIGINCO_WEBSITE_LINK} from "../constants/variables";
 import {currentYear} from "../utils/currentYear";
 
-export const Footer = ()=>{
-    return(
-        <small>Powered by <a href={DIGINCO_WEBSITE_LINK} target={"__blank"}>Diginco</a> |
-            Version 0.0.1 | Copyright- {currentYear}</small>
-    )
-}
+export const Footer = ()=>
+    <small>
+        Powered by
+        <a href={DIGINCO_WEBSITE_LINK} target={"__blank"}>Diginco</a>
+        | Version 0.0.1 | Copyright- {currentYear}
+    </small>
+
+
 
