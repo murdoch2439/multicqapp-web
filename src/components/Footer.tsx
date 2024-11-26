615839160a6145817c884de8d8f23813bea53c36
murdoch <murdochkhallz@gmail.com>
2024-11-26 20:33:58 +0100

    Adding Translation feature

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import {currentYear} from "../utils/currentYear";
 export const Footer = ()=>
     <small>
         Powered by
-        <a href={DIGINCO_WEBSITE_LINK} target={"__blank"}>Diginco</a>
+        <a href={DIGINCO_WEBSITE_LINK} target={"__blank"}> Diginco </a>
         | Version 0.0.1 | Copyright- {currentYear}
     </small>
 
