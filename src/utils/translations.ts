1f5314196337a3b5efa5af22fbc33263af3fb86f
murdoch <murdochkhallz@gmail.com>
2024-11-26 20:33:28 +0100

    Adding footer to the project

---
+++ src/utils/translations.ts
@@ -0,0 +1,61 @@
+// src/translations.ts
+export const translations = {
+    en: {
+        questions: [
+            {
+                question: 'What was the name of the first electronic computer, built in 1946?',
+                answers: ['TERMOX', 'MADAV', 'ENIAC', 'EDVAC'],
+                correctAnswer: 'ENIAC',
+            },
+            {
+                question: 'Which operating system was developed by Linus Torvalds?',
+                answers: ['Windows', 'macOs', 'Android', 'Linux'],
+                correctAnswer: 'Linux',
+            },
+            {
+                question: 'What protocol is used to transfer files over the web?',
+                answers: ['HTML', 'HTTP', 'FTP', 'SMTP'],
+                correctAnswer: 'FTP',
+            },
+            {
+                question: "What's the main difference between IPv4 and IPv6?",
+                answers: ['Security','IP address size' , 'Data encryption', 'Connection speed'],
+                correctAnswer: 'IP address size',
+            },
+        ],
+        titles:[
+            {
+                h1:"Test your level in TI!"
+            }
+        ]
+    },
+    fr: {
+        questions: [
+            {
+                question: 'Quel est le nom du premier ordinateur électronique, construit en 1946?',
+                answers: ['TERMOX', 'MADAV', 'ENIAC', 'EDVAC'],
+                correctAnswer: 'ENIAC',
+            },
+            {
+                question: "Quel système d'exploitation a été développé par Linus Torvalds?",
+                answers: ['Windows', 'macOs', 'Android', 'Linux'],
+                correctAnswer: 'Linux',
+            },
+            {
+                question: 'Quel est le protocole utilisé pour transférer des fichiers sur le web?',
+                answers: ['HTML', 'HTTP', 'FTP', 'SMTP'],
+                correctAnswer: 'FTP',
+            },
+            {
+                question: "Quelle est la principale différence entre IPv4 et IPv6?",
+                answers: ["Sécurité", "IP address size" , "Encryption des donnée", "Connexion rapide"],
+                correctAnswer: "IP address size",
+            },
+        ],
+    },
+    titles:[
+        {
+            h1:"Tester votre niveau en IT!"
+        }
+    ]
+};
