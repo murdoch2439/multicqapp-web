ec7f25a38d77b7d183719c3d471730228ec10d23
murdoch <murdochkhallz@gmail.com>
2024-11-26 16:51:36 +0100

    Layout setup

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,16 @@
+import "./progressBar.style.css"
+import {FC} from "react";
+type props ={
+    progressPercentage:number,
+    quizFinished?:boolean
+}
+export const ProgressBar:FC<props> =({progressPercentage})=>{
+    return(
+        <div className={"progressbar-container"}>
+            <div className={progressPercentage < 100 ? "progressbar-step-less-than-hundred" : "progressbar-step"} style={{
+                width: `${progressPercentage}%`,
+                transition: 'width 0.3s',
+            }}></div>
+        </div>
+    )
+}
